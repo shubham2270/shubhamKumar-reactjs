f334e51a9b7cb746bea19414973ea10890767df7
Shubham Kumar <shubham.kumar@aicrowd.com>
2022-08-12 15:11:25 +0530

    added product detail page

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -1,29 +1,32 @@
 import React from "react";
+import Link from "next/link";
 
 const ProductCard = ({}) => {
   return (
-    <div className='w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
-      <a href='#'>
-        <img
-          className='p-8 rounded-t-lg'
-          src='/docs/images/products/apple-watch.png'
-          alt='product image'
-        ></img>
-      </a>
-      <div className='lg:px-5 lg:pb-5 px-3 pb-3'>
-        <a href='#'>
-          <h5 className='text-l lg:text-xl lg:font-semibold tracking-tight text-gray-900 dark:text-white'>
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
-        </a>
+    <Link href={`/products/45`}>
+      <a>
+        <div className='w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
+          <img
+            className='p-8 rounded-t-lg'
+            src='https://via.placeholder.com/150'
+            alt='product image'
+          ></img>
+          <div className='lg:px-5 lg:pb-5 px-3 pb-3'>
+            <a href='#'>
+              <h5 className='text-l lg:text-xl lg:font-semibold tracking-tight text-gray-900 dark:text-white'>
+                Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+              </h5>
+            </a>
 
-        <div className='flex justify-between items-center'>
-          <span className='text-2xl lg:text-3xl  font-bold text-gray-900 dark:text-white'>
-            $599
-          </span>
+            <div className='flex justify-between items-center'>
+              <span className='text-2xl lg:text-3xl  font-bold text-gray-900 dark:text-white'>
+                $599
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 };
 
