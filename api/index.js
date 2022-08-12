6bfd85399772952378a50fd14a10781db8d843d2
Shubham Kumar <shubham.kumar@aicrowd.com>
2022-08-12 20:46:33 +0530

    hard coded token

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 // -- Products api's --
 
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNodWJoYW0yMjcwQGdtYWlsLmNvbSIsImdpdGh1YiI6Imh0dHBzOi8vZ2l0aHViLmNvbS9zaHViaGFtMjI3MCIsImlhdCI6MTY2MDE5MjQ2NSwiZXhwIjoxNjYwNjI0NDY1fQ.JhSdZ3Z8MtXtAbiPq1B5TNshmKshAs1Eli2uK2hFNjc";
+
 export const listProducts = async () => {
   const getProductUrl =
     "https://upayments-studycase-api.herokuapp.com/api/products";
   const headers = {
-    Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_TOKEN,
+    Authorization: "Bearer " + token,
     "Content-Type": "application/json",
   };
 
@@ -16,7 +19,7 @@ export const listProducts = async () => {
 export const getProduct = async (id) => {
   const getProductUrl = `https://upayments-studycase-api.herokuapp.com/api/products/${id}`;
   const headers = {
-    Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_TOKEN,
+    Authorization: "Bearer " + token,
     "Content-Type": "application/json",
   };
 
@@ -28,7 +31,7 @@ export const getProduct = async (id) => {
 export const addProduct = async (productData) => {
   const getProductUrl = `https://upayments-studycase-api.herokuapp.com/api/products/`;
   const headers = {
-    Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_TOKEN,
+    Authorization: "Bearer " + token,
     "Content-Type": "application/json",
   };
 
@@ -47,7 +50,7 @@ export const listCategories = async () => {
   const getCategoriesUrl =
     "https://upayments-studycase-api.herokuapp.com/api/categories/";
   const headers = {
-    Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_TOKEN,
+    Authorization: "Bearer " + token,
     "Content-Type": "application/json",
   };
 
@@ -59,7 +62,7 @@ export const listCategories = async () => {
 export const getCategory = async (id) => {
   const getCategoryUrl = `https://upayments-studycase-api.herokuapp.com/api/categories/${id}`;
   const headers = {
-    Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_TOKEN,
+    Authorization: "Bearer " + token,
     "Content-Type": "application/json",
   };
 
